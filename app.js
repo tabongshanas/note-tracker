ab2de106389ea07a5797fe5a0258f6ca6ba41602
Shanas Tabong <tabongshanas@gmail.com>
2026-02-10 23:53:24 +0100

    adding favicon to all pages from the _head pug file

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express();
 
 // GLOBAL MIDDLEWARE
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(cookieParser());
 
 app.set('view engine', 'pug');
